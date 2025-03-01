5b01503b9d07394946c340fad5bf8ff8472c143d
PradumnO9 <pradumn2999@gmail.com>
2025-03-01 19:15:03 +0530

    Feed API done

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { userAuth } = require("../middleware/auth");
 const ConnectionRequestsModel = require("../models/connectionRequest");
+const UserModel = require("../models/user");
 
 const router = express.Router();
 
@@ -26,6 +27,7 @@ router.get("/requests/received", userAuth, async (req, res) => {
   }
 });
 
+// Getting all the accepted connection requests for the loggedIn user
 router.get("/connections", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
@@ -39,8 +41,6 @@ router.get("/connections", userAuth, async (req, res) => {
       .populate("fromUserId", USER_SAFE_DATA)
       .populate("toUserId", USER_SAFE_DATA);
 
-      // console.log(connectionRequests)
-
     const data = connectionRequests.map((row) => {
       if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
         return row.toUserId;
@@ -49,6 +49,46 @@ router.get("/connections", userAuth, async (req, res) => {
     });
 
     res.json({ data });
+  } catch (err) {
+    res.status(400).send("ERROR : " + err.message);
+  }
+});
+
+// User feed
+router.get("/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
+
+    // Find all the connection request (sent + received)
+    const connectionRequests = await ConnectionRequestsModel.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId status");
+
+    const hideUsersFromFeed = new Set();
+
+    connectionRequests.forEach((request) => {
+      hideUsersFromFeed.add(request.fromUserId.toString());
+      hideUsersFromFeed.add(request.toUserId.toString());
+    });
+
+    // $nin => not in this array
+    // $ne => not equal
+    const users = await UserModel.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUsersFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
+
+    res.send(users);
   } catch (err) {
     res.status(400).send("ERROR : " + err.message);
   }
